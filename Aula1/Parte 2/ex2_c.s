23e630bf1f5e42af9072834b8b75600fe5551b64
José Reis <jreis@ua.pt>
2020-02-19 17:19:53 +0000

    modified:   Aula1/Parte 2/.ex2_c.s.swp 	deleted:    Aula1/Parte 2/ex2_b.elf 	deleted:    Aula1/Parte 2/ex2_b.o 	new file:   Aula1/Parte 2/ex2_c.elf 	renamed:    Aula1/Parte 2/ex2_b.hex -> Aula1/Parte 2/ex2_c.hex 	renamed:    Aula1/Parte 2/ex2_b.map -> Aula1/Parte 2/ex2_c.map 	new file:   Aula1/Parte 2/ex2_c.o 	modified:   Aula1/Parte 2/ex2_c.s

--- Aula1/Parte 2/ex2_c.s
+++ Aula1/Parte 2/ex2_c.s
@@ -4,7 +4,17 @@
 	.text
 	.globl main
 
-main:	li $t0, 0	# char c;
+main:	li $t0, 0		# char c;
 	
-while1:	beq $t0, '\n', exit
-	li 
+while1:	beq $t0, '\n', exit	# while( c != '\n'){
+while2:	li $v0, INKEY		# 
+	syscall			#
+	move $t0, $v0		# c = inkey();
+	beq $t0, 0, while2	# while( (c = inkey()) == 0 );
+if:	beq $t0, '\n', endif	# if( c!= '\n')
+	move $a0, $t0		#    putChar(c);
+	li $v0,PUT_CHAR		#
+	syscall			#
+endif:	j while1		# }
+
+exit:	jr $ra			# return 0;
