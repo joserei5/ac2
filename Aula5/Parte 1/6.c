eed3ba0e14b632d89f7dd41121eb6544a71a9e2b
Jose Reis <jreis@ua.pt>
2020-04-03 15:24:31 +0100

    Aula5: alternativa ao osciloscopio

---
+++ Aula5/Parte 1/6.c
@@ -0,0 +1,59 @@
+#include <detpic32.h>
+
+void delay(int ms)
+{
+  for(; ms>0; ms--)
+  {
+    resetCoreTimer();
+    while(readCoreTimer() < 20000);
+  }
+}
+
+void ConfigureAll()
+{
+  /* I/O Configuration */
+  LATEbits.LATE0 = 0;   // RE0 MASK
+  TRISEbits.TRISE0 = 1; // RE0 = output
+
+  /* ADC Configuration */
+  LATBbits.LATB4 = 0;     // RB4 MASK
+  TRISBbits.TRISB4 = 1;   // RB4 = input
+  AD1PCFGbits.PCFG4 = 0;  // RB4 = analog input
+  AD1CHSbits.CH0SA = 4;   // AN4 MUX entry
+  AD1CON2bits.SMPI = 1-1; // Conversions: 1
+  AD1CON1bits.SSRC = 7;     // Conversion Trigger Selection Bits
+  AD1CON1bits.CLRASAM = 1;  // STOPS conversion & CLEARS ASAM bit
+                            // when an A/D interrupt is generated
+  AD1CON3bits.SAMC = 16;    // SAMPLING TIME = 16 TAD = 16*100ns
+  AD1CON1bits.ON = 1;     // ENABLE A/D Converter
+}
+
+int main(void)
+{
+  int tmrVal;
+
+  ConfigureAll();
+
+  PR5 = 65535;
+  T5CON |= 0x8000;
+
+  while(1)
+  {
+    TMR5 = 0;
+
+    AD1CON1bits.ASAM = 1;       // START Conversion
+    while(IFS1bits.AD1IF == 0); // WAIT Conversion
+    tmrVal = TMR5;
+
+    printStr("\nConv. Time=");
+    printInt10(tmrVal * 50);
+    printStr(" ns - ");
+    printInt10(ADC1BUF0);
+
+    IFS1bits.AD1IF = 0;  // RESET Conversion
+
+    delay(500);
+  }
+
+  return 0;
+}
